bcade1dfef9114c4d232892ef002f445c9983f4d
danielnahun <danielnahuncalcinafuentes@gmail.com>
2026-06-27 22:51:01 -0500

    create db structure for testing

---
+++ docker/init/001_schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE user(
+    id_user INT NOT NULL AUTO_INCREMENT PRIMERY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    token TEXT,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_by INT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deteled_by INT,
+    deteled_at TIMESTAMP
+);
+
+CREATE TABLE notifications(
+    id_notification INT NOT NULL AUTO_INCREMENT PRIMERY KEY,
+    sender_id INT,
+    receiver_id INT,
+    sender_contact VARCHAR(255),
+    receiver_contact VARCHAR(255),
+    subject VARCHAR(255),
+    message TEXT,
+    channel ENUM('email','sms','push'),
+    status ENUM('pending','sent','failed'),
+    created_by INT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deteled_by INT,
+    deteled_at TIMESTAMP
+)
